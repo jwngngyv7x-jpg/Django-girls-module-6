ca0af32424ab2a466cc4ac9f92af3e80626eb27c
Sarah Gallagher <jwngngyv7x@privaterelay.appleid.com>
2026-05-03 21:14:14 -0400

    Remove automatic published date on post save
    Removed setting of published_date to current time when saving a post.

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -28,7 +27,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
